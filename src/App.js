7aea2fb77f1a4161f8b4cbf3b01ffcd01d6af2a3
SeanBell18 <sean.bell182@gmail.com>
2018-10-16 10:23:40 -0600

    setup finished with installed packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import './App.css';
+import AdminOverview from './components/Overview/AdminOverview'
+import UserOverview from './components/Overview/UserOverview'
+import Calendar from './components/Calendar/Calendar'
+import Home from './components/Home/Home'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isAdmin = false,
+      isUser = false
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter >
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='' component={} />
+          if (isAdmin) {
+            <div>
+              <Route path='/overview' component={AdminOverview} />
+              <Route path='/calendar' component={Calendar} />
+              <Route path='/store' component={Store} />
+            </div>
+          } else if (isUser) {
+            <div>
+              <Route path='/overview' component={UserOverview} />
+              <Route path='/calendar' component={Calendar} />
+              <Route path='/store' component={Store} />
+            </div>
+          }
+        </Switch>
+      </HashRouter>
     );
   }
 }
